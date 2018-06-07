67e444a5697c9b33ea503de592027741a7663c43
finify <ifeanyiizuegbu@yahoo.com>
2018-06-07 22:11:37 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <title>Ifeanyi Izuegbu | Portfolio </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="description" content="Portfolio">
+  <!-- Open Graph data -->
+  <meta property="og:title" content="portfolio ">
+  <meta property="og:type" content="article">
+  <meta property="og:description" content="portfolio">
+
+  <link rel="canonical" href="index.html">
+  <meta property="og:url" content="index.html">
+  <meta property="og:site_name" content="Ifeanyi Izuegbu">
+  <link href="images/favicon/favicon-32.png" rel="shortcut icon" type="image/png">
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,700,900" rel="stylesheet">
+  <link rel="stylesheet" href="css/animate.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css">
+
+<style>
+.tech_skill{
+  margin-top:20px;
+  width:100%;
+  overflow:auto;
+}
+#skill_con{
+  width:82%;
+  height:100%;
+  margin-left:8%;
+}
+#skill_con b{
+  color:#252a30;
+  font-size: 3.2rem;
+}
+#skill_con b1{
+  color:#524e4d;
+  font-size: 2.1rem;
+}
+#skill_con ul{
+  text-align:left;
+  font-size: 1.6rem;
+  color:#524e4d;
+}
+</style>
+
+</head>
+
+<body>
+  <div id="wrapper">
+    <header></header>
+    <!-- Start Hero Section-->
+    <div id="fullscreen-hero" class="home-hero">
+      <div class="hero-container">
+        <div class="hero-content">
+
+          <h4 class="cd-headline letters type" style="color:white;">
+            <span>Hello sir, I'm Ifeanyi izuegbu. I am a software developer</span>
+            <br>
+            <span class="cd-words-wrapper">
+              <b class="is-visible">I Design and Code..</b>
+              <b>Web applications</b>
+              <b>Desktop applications</b>
+              <b>Websites</b>
+              <b>Mobile apps</b>
+            
+              
+            </span>
+          </h4>
+          <div class="page-scroll">
+            <a href="http://goo.gl/e2d2yu" class="button button-ghost animated fadeInLeft" style="background-color:red;">Hire Me</a>
+          </div>
+          <h3 id="typed"></h3>
+
+          <div class="page-scroll learn-more">
+            <a href="#work">My Portfolio
+              <br>
+              <i class="fa fa-chevron-down"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Start Experience Section-->
+    <div id="aboutMe" class="bounce" style="justify-content:center;">
+      <p class="intro slate" class="bounce" style="margin:0 auto;justify-content:center;">About Me</p>
+    </div>
+    <div class="container" id="aboutMe">
+      <p class="statement gunmetal" style="margin:0 auto;font-size:20px;text-align:left;">
+        I'm a Software developer with over three(3) years of experience in web and software development. I am a self taught programmer.
+        I've been investing an insane amount of hours in self development, ever ready to learn, build, ship and improve top class software solutions using world class tools. I have a strong
+        passion for technology especially software. I'm also a good team player with excellent commmunication skills. I'm
+        enthusiastic about using Technology to solve real world problems and i'm ever ready to be involved in building the
+        next big thing if i'm given the opportunity to work for your company sir.
+      </p>
+    </div>
+
+    <div class="tech_skill">
+      <div id="skill_con">
+      <center>
+        <b>Technical Skils</b><br>
+        <b1>Programming Languages/Devop tools</b1><br>
+        <ul>
+          <li>HTML,CSS,JAVASCRIPT</li>
+          <li>BOOTSTRAP,JQUERRY,JQUERRY UI,ANGULAR JS</li>
+          <li>PHP,MYSQLI, AJAX</li>
+          <li>VASUAL BASIC,.NET FRAMEWORK</li>
+          <li>GIT,GITHUB,SOURCE TREE,VISUAL STUDIO CODE,TRELLO,SLACK(devop tools)</li>
+        </ul>
+      </center>
+      </div>
+    </div>
+
+
+
+    <section id="me" class="fadeInRight>
+      <div class=" container ">
+        <div class="row ">
+          <div class="eight columns offset-by-two columns ">
+            <p class="intro slate ">My selling points</p>
+          </div>
+        </div>
+        <div class="row ">
+          <div class="one-third column ">
+            <div class="expertise-block ">
+              <i class="fa fa-map-o yellow "></i>
+              <h4 class="title slate ">Strategy</h4>
+              <p class="summary" style="font-size:2rem;">I collaborate with clients and peers to transform their ideas into products and their passion into businesses with good user experiences.</p>
+            </div>
+          </div>
+          <div class="one-third column ">
+            <div class="expertise-block ">
+              <i class="fa fa-magic yellow "></i>
+              <h4 class="title slate ">Design</h4>
+              <p class="summary" style="font-size:2rem;">I sketch interfaces focusing on content structure, intuitive UI patterns and simple interactions.
+                I'm a minimalist who truly believes that less is more and that simplicity is the ultimate sophistication.</p>
+            </div>
+          </div>
+          <div class="one-third column ">
+            <div class="expertise-block ">
+              <i class="fa fa-code yellow "></i>
+              <h4 class="title slate ">Code</h4>
+              <p class="summary" style="font-size:2rem;">I design with HTML5, CSS3, JavaScript ES5 and for my backend i use PHP & Ajax. I love coding from the scratch, but I can work with
+                front-end frameworks like Bootstrap, jquerry and Angular</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Start Work Section-->
+    <div id="work">
+      <div class="container">
+        <div class="row">
+          <div class="twleve columns ">
+            <p class="intro slate ">My Portfolio</p>
+           
+          </div>
+        </div>
+        <section class="project-showcase">
+          <div class="row">
+            <div class="six columns u-pull-right ">
+              <img src="images/elibrary1.png " style="border-radius: 10px; " class="project-image ">
+            </div>
+            <div class="six columns u-pull-left ">
+              <div class="project-content text-left ">
+                <h3 class="project-title ">Elibary</h3>
+                <ul class="project-tags ">
+                  <li>Html,Css,JS,bootstrap</li>
+                  <li>Php,ajax</li>
+                </ul>
+                <p class="project-description ">This is an elibrary where admin can upload materials and users can access them.</p>
+                <a href="http://www.github.com/finify/elibrary" target="_blank "
+                  class="button button-primary ">View Git Repo</a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="project-showcase ">
+          <div class="row ">
+            <div class="six columns u-pull-left ">
+              <img src="images/bookshop1.png " class="project-image ">
+            </div>
+            <div class="six columns u-pull-right ">
+              <div class="project-content text-right ">
+                <h3 class="project-title ">Bookshop solution</h3>
+                <ul class="project-tags ">
+                  <li>Html,Css,bootstrap</li>
+                  <li>Php,ajax</li>
+                </ul>
+                <p class="project-description ">A web app solution that help bookshop owners keep inventory of there book stock</p>
+                <a href="http://www.github.com/finify/bookshop" target="_blank "
+                  class="button button-primary ">View Repo on Github</a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="project-showcase">
+          <div class="row ">
+            <div class="six columns u-pull-right ">
+              <img src="images/ponzi1.png" class="project-image ">
+            </div>
+            <div class="six columns u-pull-left ">
+              <div class="project-content text-left ">
+                <h3 class="project-title ">A ponzi Website</h3>
+                <ul class="project-tags ">
+                  <li>Html,Css,bootstrap</li>
+                  <li>Php,ajax</li>
+                </ul>
+                <p class="project-description ">A ponzi website i coded from scratch to improve my php and ajax skills,the demo is not available,it runs on my local xampp server</p>
+                <a href="#" target="_blank " class="button button-primary ">No demo</a>
+              </div>
+            </div>
+          </div>
+        </section>
+           </div>
+    </div>
+    <!-- Start sub-footer-->
+    
+  <footer id="footer ">
+    <div id="contact ">
+      <div class="container ">
+        <div class="row ">
+          <div class="twelve columns ">
+              <img width="200px" src="images/mypic.jpg ">
+            <p class="made-by-me ">Ifeanyi Izuegbu, 2018</p>
+            <p>Here are links to my social media profile</p>
+            <div class="social-links ">
+              <a href="http://www.twitter.com/IzuegbuIfeanyi2?s=08">
+                <i class="fa fa-twitter "></i>
+              </a>
+              <a href="http://www.linkedin.com/in/ifeanyichukwu-izuegbu-31154414b" target="_blank ">
+                <i class="fa fa-linkedin "></i>
+              </a>
+              <a href="http://www.github.com/finify" target="_blank ">
+                <i class="fa fa-github "></i>
+              </a>
+              <a href="http://www.facebook.com/ifeanyi.izuegbu1" target="_blank ">
+                <i class="fa fa-facebook "></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  </div>
+
+  <script src="js/jquery.js "></script>
+  <script src="../cdnjs.cloudflare.com/ajax/libs/typeit/5.0.1/typeit.min.js "></script>
+  <script src="js/jquery.easing.1.3.min.js "></script>
+  <script src="js/jquery.scrollto.min.js "></script>
+  <script src="js/custom.min.js "></script>
+  <script src="js/wow.min.js "></script>
+  <script src="js/velocity.min.js "></script>
+  <script src="js/typeit.js "></script>
+  <script>
+    new WOW().init();
+  </script>
+
+
+</body>
+
+</html>
